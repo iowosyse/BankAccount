6a8e4628743f2a83191cbbee5fe13ae6511ee497
iowosyse <iowosyse@gmail.com>
2024-05-20 09:31:07 -0600

    20-05-2024

--- src/AuxExceptions/NoNegativeOperationException.java
+++ src/AuxExceptions/NoNegativeOperationException.java
@@ -2,6 +2,7 @@ package AuxExceptions;
 
 public class NoNegativeOperationException extends RuntimeException{
     public NoNegativeOperationException() {
+        super();
     }
 
     public NoNegativeOperationException(String message) {
